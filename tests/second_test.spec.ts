608d9beefa63dc2118d3ac9efa6f372261ca6b2a
660710611 <pulkasem_p@silpakorn.edu>
2026-02-27 22:22:02 +0700

    first try playwright

---
+++ tests/second_test.spec.ts
@@ -0,0 +1,18 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com/');
+  await page.locator('[data-test="username"]').click();
+  await page.locator('[data-test="username"]').fill('standard_user');
+  await page.locator('[data-test="password"]').click();
+  await page.locator('[data-test="password"]').fill('secret_sauce');
+  await page.locator('[data-test="login-button"]').click();
+
+  await page.locator('[data-test="product-sort-container"]').selectOption('za');
+  await page.locator('[data-test="add-to-cart-sauce-labs-backpack"]').click();
+  expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+  await page.locator('[data-test="add-to-cart-sauce-labs-bike-light"]').click();
+  expect(page.locator('.shopping_cart_badge')).toHaveText('2');
+  await page.locator('[data-test="remove-sauce-labs-backpack"]').click();
+  expect(page.locator('.shopping_cart_badge')).toHaveText('1');
+});
